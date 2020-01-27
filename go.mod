2c76d560492ce64417310f673f31f41ab0151b1d
Jonas Sala <jonasala@gmail.com>
2020-01-27 07:43:30 -0300

    adiçao de funcionalidade de microservices

--- go.mod
+++ go.mod
@@ -5,7 +5,11 @@ go 1.13
 require (
 	github.com/Masterminds/squirrel v1.1.0
 	github.com/friendsofgo/errors v0.9.2 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/gofrs/uuid v3.2.0+incompatible // indirect
+	github.com/hashicorp/consul/api v1.3.0
+	github.com/lib/pq v1.3.0 // indirect
+	github.com/mattn/go-sqlite3 v2.0.2+incompatible // indirect
 	github.com/stretchr/testify v1.4.0
 	github.com/volatiletech/inflect v0.0.0-20170731032912-e7201282ae8d // indirect
 	github.com/volatiletech/null v8.0.0+incompatible
---
+++ service/service.go
@@ -0,0 +1,99 @@
+package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+
+	consul "github.com/hashicorp/consul/api"
+)
+
+//Service agrega as configurações de banco de dados (pgsql) e service discovery (consul)
+type Service struct {
+	Name        string
+	Config      Config
+	ConsulAgent *consul.Agent
+	ConsulKV    *consul.KV
+}
+
+//Config representa as configurações de que vem do consul
+type Config struct {
+	DBHost     string `json:"db_host"`
+	DBName     string `json:"db_name"`
+	DBUser     string `json:"db_user"`
+	DBPassword string `json:"db_password"`
+	DBPort     string `json:"db_port"`
+	DBSchema   string `json:"db_schema"`
+	HTTPPort   string `json:"http_port"`
+	TTL        string `json:"ttl"`
+}
+
+//New cria e configura um serviço
+func New(name, configKey string) (*Service, error) {
+	service := &Service{Name: name}
+
+	client, err := consul.NewClient(consul.DefaultConfig())
+	if err != nil {
+		return service, fmt.Errorf("não foi possível conectar com o consul. %v", err)
+	}
+
+	service.ConsulKV = client.KV()
+
+	pair, _, err := service.ConsulKV.Get(configKey, nil)
+	if err != nil {
+		return service, fmt.Errorf("problemas ao recuperar a chave %v. %v", configKey, err)
+	}
+
+	if pair == nil {
+		return service, fmt.Errorf("chave %v inexistente", configKey)
+	}
+
+	config := Config{}
+	if err := json.Unmarshal(pair.Value, &config); err != nil {
+		return service, fmt.Errorf("a chave %v não é um json válido. %v", configKey, err)
+	}
+
+	service.Config = config
+
+	service.ConsulAgent = client.Agent()
+
+	return service, nil
+}
+
+//RegisterService registra o serviço no consul.
+//O parâmetro healthCheckFunction deve ser uma função que retorna um erro se houver falha
+func (s *Service) RegisterService(healthcheckFunction func() error) error {
+	ttl, err := time.ParseDuration(s.Config.TTL)
+	if err != nil {
+		return fmt.Errorf("não foi possível interpretar o ttl. %v", err)
+	}
+
+	port, _ := strconv.Atoi(s.Config.HTTPPort)
+
+	def := &consul.AgentServiceRegistration{
+		Name: s.Name,
+		Port: port,
+		Check: &consul.AgentServiceCheck{
+			TTL: ttl.String(),
+		},
+	}
+
+	if err = s.ConsulAgent.ServiceRegister(def); err != nil {
+		return fmt.Errorf("não foi possível registrar o serviço no consul. %v", err)
+	}
+
+	go func() {
+		ticker := time.NewTicker(ttl / 2)
+		for range ticker.C {
+			err := healthcheckFunction()
+			if err != nil {
+				s.ConsulAgent.FailTTL("service:"+s.Name, err.Error())
+			} else {
+				s.ConsulAgent.PassTTL("service:"+s.Name, "")
+			}
+		}
+	}()
+
+	return nil
+}
