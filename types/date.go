46c275207d0910f5a2dd78abe749258e883c8197
Jonas Sala <jonasala@gmail.com>
2020-06-02 22:25:50 -0300

    melhorias no type.Date para permitir valores nulos e evitar crashes

--- types/date.go
+++ types/date.go
@@ -3,6 +3,7 @@ package types
 import (
 	"database/sql/driver"
 	"encoding/json"
+	"errors"
 	"strings"
 	"time"
 )
@@ -13,22 +14,51 @@ type Date time.Time
 //UnmarshalJSON implementação
 func (d *Date) UnmarshalJSON(b []byte) error {
 	s := strings.Trim(string(b), "\"")
-	t, err := time.Parse("2006-01-02", s)
-	if err != nil {
-		return err
+	if s != "" {
+		t, err := time.Parse("2006-01-02", s)
+		if err != nil {
+			return err
+		}
+		*d = Date(t)
 	}
-	*d = Date(t)
 	return nil
 }
 
 //MarshalJSON implementação
 func (d Date) MarshalJSON() ([]byte, error) {
-	return json.Marshal(time.Time(d).Format("2006-01-02"))
+	t := time.Time(d)
+	if t.IsZero() {
+		return json.Marshal("")
+	}
+	return json.Marshal(t.Format("2006-01-02"))
 }
 
 //Value implementação
 func (d Date) Value() (driver.Value, error) {
-	return time.Time(d).Format("2006-01-02"), nil
+	t := time.Time(d)
+	if t.IsZero() {
+		return nil, nil
+	}
+	return t.Format("2006-01-02"), nil
+}
+
+//Scan implementação
+func (d *Date) Scan(value interface{}) error {
+	if value != nil {
+		switch value.(type) {
+		case time.Time:
+			*d = Date(value.(time.Time))
+		case string:
+			t, err := time.Parse("2006-01-02", value.(string))
+			if err != nil {
+				return err
+			}
+			*d = Date(t)
+		default:
+			return errors.New("invalid date field")
+		}
+	}
+	return nil
 }
 
 //NewDate cria um novo Date a partir de uma string yyyy-mm-dd
